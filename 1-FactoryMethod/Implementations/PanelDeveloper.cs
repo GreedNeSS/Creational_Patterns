5954976e3b934db4d7a68f86512b45fade11b414
GreedNeSS <ruslan199126@gmail.com>
2022-09-08 21:30:04 +0300

    Added creators implementation.

---
+++ 1-FactoryMethod/Implementations/PanelDeveloper.cs
@@ -0,0 +1,21 @@
+﻿using _1_FactoryMethod.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_FactoryMethod.Implementations
+{
+    public class PanelDeveloper : Developer
+    {
+        public PanelDeveloper(string name) : base(name)
+        {
+        }
+
+        public override House Create()
+        {
+            return new PanelHouse();
+        }
+    }
+}
---
+++ 1-FactoryMethod/Implementations/WoodDeveloper.cs
@@ -0,0 +1,21 @@
+﻿using _1_FactoryMethod.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_FactoryMethod.Implementations
+{
+    public class WoodDeveloper : Developer
+    {
+        public WoodDeveloper(string name) : base(name)
+        {
+        }
+
+        public override House Create()
+        {
+            return new WoodHouse();
+        }
+    }
+}
