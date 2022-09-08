8b4be8f9547654db772e7ea5465ee6c64d60468a
GreedNeSS <ruslan199126@gmail.com>
2022-09-08 21:03:51 +0300

    Added implementations.

---
+++ 1-FactoryMethod/Implementations/PanelHouse.cs
@@ -0,0 +1,17 @@
+﻿using _1_FactoryMethod.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_FactoryMethod.Implementations
+{
+    public class PanelHouse : House
+    {
+        public PanelHouse()
+        {
+            Console.WriteLine("Панельный дом построен!");
+        }
+    }
+}
---
+++ 1-FactoryMethod/Implementations/WoodHouse.cs
@@ -0,0 +1,17 @@
+﻿using _1_FactoryMethod.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_FactoryMethod.Implementations
+{
+    public class WoodHouse : House
+    {
+        public WoodHouse()
+        {
+            Console.WriteLine("Деревяныый дом построен!");
+        }
+    }
+}
