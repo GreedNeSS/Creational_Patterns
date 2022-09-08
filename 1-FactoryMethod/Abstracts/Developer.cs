a87a2a0d8bcfe3d8c9367f368239b6dfb26c2258
GreedNeSS <ruslan199126@gmail.com>
2022-09-08 21:26:45 +0300

    Added abstract creator.

---
+++ 1-FactoryMethod/Abstracts/Developer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_FactoryMethod.Abstracts
+{
+    public abstract class Developer
+    {
+        public string Name { get; set; }
+
+        public Developer(string name)
+        {
+            Name = name;
+        }
+
+        abstract public House Create(); 
+    }
+}
