40187ba9d09d0bd2840c7fb3b0cea40712948c49
GreedNeSS <ruslan199126@gmail.com>
2022-09-11 21:36:20 +0300

    Added implementation.

---
+++ 2-AbstractFactory/Implementations/Arbalet.cs
@@ -0,0 +1,17 @@
+﻿using _2_AbstractFactory.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_AbstractFactory.Implementations
+{
+    public class Arbalet : Weapon
+    {
+        public override void Hit()
+        {
+            Console.WriteLine("Выстрел из арбалета.");
+        }
+    }
+}
---
+++ 2-AbstractFactory/Implementations/Sword.cs
@@ -0,0 +1,17 @@
+﻿using _2_AbstractFactory.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_AbstractFactory.Implementations
+{
+    public class Sword : Weapon
+    {
+        public override void Hit()
+        {
+            Console.WriteLine("Удар мечём.");
+        }
+    }
+}
