c6217a0845618e3fae6fd816ebebbe8ad7179d66
GreedNeSS <ruslan199126@gmail.com>
2022-09-11 21:31:42 +0300

    Added HeroFactory class.

---
+++ 2-AbstractFactory/Abstracts/HeroFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_AbstractFactory.Abstracts
+{
+    public abstract class HeroFactory
+    {
+        public abstract Movement CreateMovement();
+        public abstract Weapon CreateWeapon();
+    }
+}
