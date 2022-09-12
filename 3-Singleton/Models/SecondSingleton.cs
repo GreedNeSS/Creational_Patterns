826c9a9a7ded3ec262bac60265b7f0f843fa8373
GreedNeSS <ruslan199126@gmail.com>
2022-09-12 20:42:48 +0300

    Added second implementation.

--- 3-Singleton/Models/SecondSingleton.cs
+++ 3-Singleton/Models/SecondSingleton.cs
@@ -8,7 +8,26 @@ namespace _3_Singleton.Models
 {
     public class SecondSingleton
     {
-        public string? Name { get; set; }
+        public string Date { get; private set; }
+        public string Name { get; private set; }
 
+        private SecondSingleton()
+        {
+            Console.WriteLine($"Singleton ctor ${DateTime.Now.ToLongTimeString()}");
+            Date = DateTime.Now.ToLongTimeString();
+            Name = Guid.NewGuid().ToString();
+        }
+
+        private class Nested
+        {
+            static Nested() { }
+            internal static readonly SecondSingleton instance = new SecondSingleton();
+        }
+
+        public static SecondSingleton GetInstance()
+        {
+            Console.WriteLine($"Singleton GetInstance ${DateTime.Now.ToLongTimeString()}");
+            return Nested.instance;
+        }
     }
 }
