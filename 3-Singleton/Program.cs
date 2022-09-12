307875f896c2848e8bc389fda9eb9a5de4f6088d
GreedNeSS <ruslan199126@gmail.com>
2022-09-12 20:55:09 +0300

    Added example.

--- 3-Singleton/Program.cs
+++ 3-Singleton/Program.cs
@@ -1,7 +1,27 @@
 ﻿using _3_Singleton.Models;
 
+Console.WriteLine($"Used static field: {Singleton.Text}");
+
 (new Thread(() =>
 {
     Singleton firstSingleton1 = Singleton.GetInstance();
     Console.WriteLine($"firstSingleton1.Name: {firstSingleton1.Name}");
-}))
+})).Start();
+
+Singleton firstSingleton2 = Singleton.GetInstance();
+Console.WriteLine($"firstSingleton2.Name: {firstSingleton2.Name}");
+
+Console.WriteLine("\n_______________________\n");
+
+Console.WriteLine($"Used static field: {Singleton.Text}");
+
+(new Thread(() =>
+{
+    SecondSingleton secondSingleton1 = SecondSingleton.GetInstance();
+    Console.WriteLine($"secondSingleton1.Name: {secondSingleton1.Name}");
+})).Start();
+
+SecondSingleton secondSingleton2 = SecondSingleton.GetInstance();
+Console.WriteLine($"secondSingleton2.Name: {secondSingleton2.Name}");
+
+Console.ReadLine();
