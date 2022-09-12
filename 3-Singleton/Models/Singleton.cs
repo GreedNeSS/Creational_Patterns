6b2257c5c2adf8fce4e276f6c0e43f67183ec65d
GreedNeSS <ruslan199126@gmail.com>
2022-09-12 20:27:50 +0300

    Added singleton.

---
+++ 3-Singleton/Models/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Singleton.Models
+{
+    public class Singleton
+    {
+        private static readonly Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
+
+        public string? Name { get; private set; }
+
+        private Singleton()
+        {
+            Name = Guid.NewGuid().ToString();
+        }
+
+        public static Singleton GetInstance()
+        {
+            return lazy.Value;
+        }
+    }
+}
