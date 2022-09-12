bc225e6f05ae80b0faa89a8de442f62df349f3f8
GreedNeSS <ruslan199126@gmail.com>
2022-09-12 20:35:43 +0300

    Singleton modified.

--- 3-Singleton/Models/Singleton.cs
+++ 3-Singleton/Models/Singleton.cs
@@ -10,15 +10,19 @@ namespace _3_Singleton.Models
     {
         private static readonly Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
 
-        public string? Name { get; private set; }
+        public string Date { get; private set; }
+        public string Name { get; private set; }
 
         private Singleton()
         {
+            Console.WriteLine($"Singleton ctor ${DateTime.Now.ToLongTimeString()}");
+            Date = DateTime.Now.ToLongTimeString();
             Name = Guid.NewGuid().ToString();
         }
 
         public static Singleton GetInstance()
         {
+            Console.WriteLine($"Singleton GetInstance ${DateTime.Now.ToLongTimeString()}");
             return lazy.Value;
         }
     }
