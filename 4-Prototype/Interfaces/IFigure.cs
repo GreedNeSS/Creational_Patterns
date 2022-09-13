dcafd8e6a45b38d9fe3f08b03ad373e4dea2d5eb
GreedNeSS <ruslan199126@gmail.com>
2022-09-13 18:07:57 +0300

    Added interface.

---
+++ 4-Prototype/Interfaces/IFigure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Prototype.Interfaces
+{
+    public interface IFigure
+    {
+        IFigure Clone();
+        IFigure DeepClone();
+        void GetInfo();
+    }
+}
