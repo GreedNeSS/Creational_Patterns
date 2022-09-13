1d92bdb50cad4080fdae659468f14dc6d6596eea
GreedNeSS <ruslan199126@gmail.com>
2022-09-13 18:36:01 +0300

    Added new Example.

--- 4-Prototype/Program.cs
+++ 4-Prototype/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _4_Prototype.Interfaces;
+using _4_Prototype.Implementations;
+
+Rectangle rectangle = new Rectangle(100, 200, 0, 20);
+IFigure recClone = rectangle.Clone();
+IFigure recDeepClone = rectangle.DeepClone();
+
+Circle circle = new Circle(40, 0, 20);
+IFigure cirClone = circle.Clone();
+IFigure cirDeepClone = circle.DeepClone();
+
+rectangle.Point.X = 100;
+circle.Point.X = 100;
+
+rectangle.GetInfo();
+recClone.GetInfo();
+recDeepClone.GetInfo();
+
+circle.GetInfo();
+cirClone.GetInfo();
+cirDeepClone.GetInfo();
+
+Console.ReadLine();
