6a3946be0a7a274b0aaafdc955bb7b4f0ee5d45b
GreedNeSS <ruslan199126@gmail.com>
2022-09-13 18:28:22 +0300

    Added implementations.

---
+++ 4-Prototype/Implementations/Circle.cs
@@ -0,0 +1,31 @@
+﻿using _4_Prototype.Abstracts;
+using _4_Prototype.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Prototype.Implementations
+{
+    public class Circle : AbstractFigure
+    {
+        public int Radius { get; set; }
+        public Point Point { get; set; }
+
+        public Circle(int radius, int posX, int posY)
+        {
+            Radius = radius;
+            Point = new Point { X = posX, Y = posY };
+        }
+
+        public override void GetInfo()
+        {
+            string info = "\nCircle:\n";
+            info += $"\nRadius = {Radius}";
+            info += $"\nPosition = {Point.X} : {Point.Y}";
+            info += $"\n____________________\n";
+            Console.WriteLine(info);
+        }
+    }
+}
---
+++ 4-Prototype/Implementations/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using _4_Prototype.Abstracts;
+using _4_Prototype.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Prototype.Implementations
+{
+    public class Rectangle : AbstractFigure
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Point Point { get; set; }
+
+        public Rectangle(int width, int height, int posX, int posY)
+        {
+            Width = width;
+            Height = height;
+            Point = new Point() { X = posX, Y = posY };
+        }
+
+        public override void GetInfo()
+        {
+            string info = "\nRectangle:\n";
+            info += $"\nWidth = {Width}";
+            info += $"\nHeight = {Height}";
+            info += $"\nPosition = {Point.X} : {Point.Y}";
+            info += $"\n____________________\n";
+            Console.WriteLine(info);
+        }
+    }
+}
