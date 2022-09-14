37fcc79d06aefd14e1105c529d24f04761e9f123
GreedNeSS <ruslan199126@gmail.com>
2022-09-14 20:17:54 +0300

    Added two builders.

---
+++ 5-Builder/Builders/RyeBreadBuilder.cs
@@ -0,0 +1,28 @@
+﻿using _5_Builder.Abstracts;
+using _5_Builder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Builder.Builders
+{
+    public class RyeBreadBuilder : BreadBuilder
+    {
+        public override void SetAdditives()
+        {
+            Bread.Flour = new Flour { Sort = "Ржаная мука 1 сорт" };
+        }
+
+        public override void SetFlour()
+        {
+            Bread.Salt = new Salt();
+        }
+
+        public override void SetSalt()
+        {
+            
+        }
+    }
+}
---
+++ 5-Builder/Builders/WheatBreadBuilder.cs
@@ -0,0 +1,28 @@
+﻿using _5_Builder.Abstracts;
+using _5_Builder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Builder.Builders
+{
+    public class WheatBreadBuilder : BreadBuilder
+    {
+        public override void SetAdditives()
+        {
+            Bread.Flour = new Flour { Sort = "Пшеничная мука высший сорт" };
+        }
+
+        public override void SetFlour()
+        {
+            Bread.Salt = new Salt();
+        }
+
+        public override void SetSalt()
+        {
+            Bread.Additives = new Additives { Name = "ванильный сахар"};
+        }
+    }
+}
