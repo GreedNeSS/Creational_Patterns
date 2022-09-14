1d1d75890cd77027913a81d09854efc16ad6ac02
GreedNeSS <ruslan199126@gmail.com>
2022-09-14 20:00:41 +0300

    Added new model.

---
+++ 5-Builder/Models/Bread.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Builder.Models
+{
+    public class Bread
+    {
+        public Flour? Flour { get; set; }
+        public Salt? Salt { get; set; }
+        public Additives? Additives { get; set; }
+
+        public override string ToString()
+        {
+            string result = string.Empty;
+
+            if (Flour != null)
+            {
+                result += Flour.Sort + "\n";
+            }
+
+            if (Salt != null)
+            {
+                result += "Соль \n";
+            }
+
+            if (Additives != null)
+            {
+                result += "Добавки: " + Additives.Name + "\n";
+            }
+
+            return result;
+        }
+    }
+}
