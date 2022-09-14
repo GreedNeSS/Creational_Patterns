e81ff5e3b087a4b1f1409b7483b37cbce2c6f8eb
GreedNeSS <ruslan199126@gmail.com>
2022-09-14 20:22:00 +0300

    Added new example.

--- 5-Builder/Program.cs
+++ 5-Builder/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _5_Builder.Abstracts;
+using _5_Builder.Builders;
+using _5_Builder.Directors;
+using _5_Builder.Models;
+
+Baker baker = new Baker();
+BreadBuilder breadBuilder = new RyeBreadBuilder();
+Bread bread = baker.Bake(breadBuilder);
+Console.WriteLine(bread);
+
+breadBuilder = new WheatBreadBuilder();
+bread = baker.Bake(breadBuilder);
+Console.WriteLine(bread);
+
+Console.ReadLine();
