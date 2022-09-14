fc62dcfd0ad73bc800dc3415c4ca371145ee84ef
GreedNeSS <ruslan199126@gmail.com>
2022-09-14 20:09:41 +0300

    Added director.

---
+++ 5-Builder/Directors/Baker.cs
@@ -0,0 +1,21 @@
+﻿using _5_Builder.Abstracts;
+using _5_Builder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Builder.Directors
+{
+    public class Baker
+    {
+        public Bread Bake(BreadBuilder builder)
+        {
+            builder.SetFlour();
+            builder.SetSalt();
+            builder.SetAdditives();
+            return builder.Bread;
+        }
+    }
+}
