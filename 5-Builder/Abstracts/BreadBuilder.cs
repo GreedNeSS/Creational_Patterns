a536a956fa00248303e61c89e34fad388df8e6b6
GreedNeSS <ruslan199126@gmail.com>
2022-09-14 20:06:05 +0300

    Added abstract builder.

---
+++ 5-Builder/Abstracts/BreadBuilder.cs
@@ -0,0 +1,23 @@
+﻿using _5_Builder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Builder.Abstracts
+{
+    public abstract class BreadBuilder
+    {
+        public Bread Bread { get; private set; }
+
+        public BreadBuilder()
+        {
+            Bread = new Bread();
+        }
+
+        public abstract void SetFlour();
+        public abstract void SetSalt();
+        public abstract void SetAdditives();
+    }
+}
